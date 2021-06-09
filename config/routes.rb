d251ae070b9e7241bf0e6cec16d8a77ae54257b9
yoshihiro-0124 <taoyoshi0124@gmail.com>
2021-06-09 19:51:08 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  root :to => 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
