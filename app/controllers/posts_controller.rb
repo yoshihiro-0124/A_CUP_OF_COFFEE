2f453d9a968434692fe76219eddddf8c1f7f4a7f
yoshihiro-0124 <taoyoshi0124@gmail.com>
2021-06-26 21:33:18 +0000

    時間設定追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
   
   def show
+    @post = Post.find(params[:id])
   end 
   
   def index
   end
   
   def new
+    @post = Post.new
   end
   
   def create
+    @post = Post.create params.require(:post).permit(:beans, :body, :user_id, :grind_id, :making_at, :images [])
+    redirect_to root_path
   end
   
   def edit
@@ -20,7 +24,7 @@ class PostsController < ApplicationController
   
   private
   def post_params
-    params.require(:post).permit(:beans, :body, :user_id, :grind_id, :makingtime_id)
+    params.require(:post).permit(:beans, :body, :user_id, :grind_id, :making_at)
   end
   
 end
